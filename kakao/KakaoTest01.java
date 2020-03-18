b43a8405bea8abd2b3e40a626fe85e5cab9e9eb0
greenhelix <dlrghks4444@gmail.com>
2020-03-19 00:16:44 +0900

    check

---
+++ kakao/KakaoTest01.java
@@ -0,0 +1,8 @@
+public class KakaoTest01{
+    public static void main(String[] args) {
+        System.out.println("Hello, Lets Start coding"); 
+        String a = "()()"; 
+        char[] bb = a.split(""); 
+        System.out.println(Arrays.toString(bb));
+    }
+}
--- 카카오/KakaoCodingTest/src/kakao/KakaoCodingTest01.java
+++
@@ -1,7 +0,0 @@
-package kakao;
-
-public class Kakaocodingtest {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-    }
-}

907ed480008e897b0ba838a73c359886e81cb736
greenhelix <dlrghks4444@gmail.com>
2020-03-19 01:10:45 +0900

    check
    check

--- kakao/KakaoTest01.java
+++ kakao/KakaoTest01.java
@@ -1,8 +1,39 @@
+import java.util.*;
+
 public class KakaoTest01{
-    public static void main(String[] args) {
-        System.out.println("Hello, Lets Start coding"); 
-        String a = "()()"; 
-        char[] bb = a.split(""); 
-        System.out.println(Arrays.toString(bb));
+    public static void main( String[] args) {
+     String s ="()()" ;    
+     System.out.println(s); 
     }
+
+    // stack을 활용한다. 
+    public boolean isCorrect(String str){
+            boolean ref = true; 
+            int left = 0, right = 0; 
+            Stack<Character> mystack = new Stack<Character>();
+
+        for(int i = 0; i<str.length(); ++i){
+            if(str.charAt(i)== '('){
+                left++;
+                mystack.push('(');
+
+            }else{
+                right++;
+                if(mystack.empty())
+                ref = false;
+                else
+                    mystack.pop();
+            }
+            if(left==right){
+                
+            }
+        }
+    }
+    public String solution(String s){
+        if(s.isEmpty()) return  s;
+
+        String answer = ""; 
+        return answer;
+    }
+
 }

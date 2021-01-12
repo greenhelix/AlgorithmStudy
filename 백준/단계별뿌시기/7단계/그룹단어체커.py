e87665ec7a0dbb34b87578eacf6dd676886c394f
greenhelix <dlrghks4444@gmail.com>
2021-01-12 21:36:45 +0900

    반복문 공부

--- 백준/단계별뿌시기/7단계/그룹단어체커.py
+++ 백준/단계별뿌시기/7단계/그룹단어체커.py
@@ -1,2 +1,16 @@
 # https://www.acmicpc.net/problem/1316
 # 구현 / 문자열
+case = int(input())
+count = 0
+for i in range(case):
+    word = input()
+    for j in range(len(word)):
+        if j != len(word)-1:
+            if word[j] == word[j + 1]:
+                pass
+            elif word[j] in word[j + 1:]:
+                break
+        else:
+            count += 1
+
+print(count)

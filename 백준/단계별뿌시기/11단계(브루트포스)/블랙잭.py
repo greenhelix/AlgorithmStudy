3f8e3018abd2180abbc2a5286bb18905277aef29
greenhelix <dlrghks4444@gmail.com>
2021-01-19 13:42:18 +0900

    수학 풀이 - 단위 값에 따른 몫 유추

--- 백준/단계별뿌시기/11단계(브루트포스)/블랙잭.py
+++ 백준/단계별뿌시기/11단계(브루트포스)/블랙잭.py
@@ -1,6 +1,7 @@
 # https://www.acmicpc.net/problem/2798
 # 세 장의 카드를 고르는 모든 경우를 고려하는 문제
 # 브루트포스 알고리즘 (https://namu.wiki/w/%EB%B8%8C%EB%A3%A8%ED%8A%B8%20%ED%8F%AC%EC%8A%A4)
+# 완전탐색
 
 
 # 이 풀이로 풀면, 런타임 에러 발생

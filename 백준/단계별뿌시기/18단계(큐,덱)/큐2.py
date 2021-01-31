06bb0f3ca5b0b9376277aec0289419acc90e1f93
greenhelix <dlrghks4444@gmail.com>
2021-01-31 17:52:07 +0900

    deque 풀이

---
+++ 백준/단계별뿌시기/18단계(큐,덱)/큐2.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/18258
+from sys import stdin
+from collections import deque
+
+n = int(stdin.readline())
+q = deque([])
+for i in range(n):
+    s = stdin.readline().split()
+    if s[0] == 'push':
+        q.append(s[1])
+    elif s[0] == 'pop':
+        if not q:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif s[0] == 'size':
+        print(len(q))
+    elif s[0] == 'empty':
+        if not q:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == 'front':
+        if not q:
+            print(-1)
+        else:
+            print(q[0])
+    elif s[0] == 'back':
+        if not q:
+            print(-1)
+        else:
+            print(q[-1])

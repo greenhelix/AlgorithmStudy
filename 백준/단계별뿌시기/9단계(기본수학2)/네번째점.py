c1de4bfa1aa2dbffeebbccb0fd6f39e54a98502a
greenhelix <dlrghks4444@gmail.com>
2021-03-04 15:28:41 +0900

    곱집합, 차집합은 itertools product 와 Set상태로 차집합이 가능하다

---
+++ 백준/단계별뿌시기/9단계(기본수학2)/네번째점.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/3009
+# 직사각형 구성중 네번째 점 구하기
+from itertools import product
+x, y = set(), set()
+spot = []
+for i in range(3):
+    a, b = map(int, input().split())
+    x.add(a)
+    y.add(b)
+    spot.append((a, b))
+all = list(product(list(x), list(y)))
+answer = list(set(all) - set(spot))
+print(answer[0][0], answer[0][1])

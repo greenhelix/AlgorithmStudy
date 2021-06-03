38b4f56dc9e588f4b59d341a2586b86ba7b9c008
Kevin <dlrghks4444@gmail.com>
2021-06-03 19:46:23 +0900

    BFS -활용 나이트의 이동 문제 풀이 완료

---
+++ 백준/단계별뿌시기/23단계(DFS,BFS)/나이트의이동.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+
+def bfs(sx, sy, fx, fy):
+    vectorx = [-2, 2, -1, 1, -2, 2, -1, 1]
+    vectory = [-1, 1, -2, 2, 1, -1, 2, -2]
+    queue = deque()
+    queue.append([sx, sy])
+    board[sx][sy] = 1
+
+    while queue:
+        x, y = queue.popleft()
+
+        # 반복을 빠져나오게 해주는게 좋다.
+        if x == fx and y == fy:
+            return board[x][y] - 1
+
+        for i in range(8):
+            xx = x + vectorx[i]
+            yy = y + vectory[i]
+
+            if 0 <= xx < n and 0 <= yy < n and board[xx][yy] == 0:
+                board[xx][yy] = board[x][y] + 1
+                queue.append((xx, yy))
+
+
+test = int(input())
+while test:
+
+    n = int(input())
+    board = [[0] * n for _ in range(n)]
+    sx, sy = map(int, input().split())
+    fx, fy = map(int, input().split())
+
+    # 불필요한 테스트케이스는 빠르게 넘어갈 수 있게 해준다.
+    if sx == fx and sy == fy:
+        print(0)
+        test -= 1
+        continue
+
+    print(bfs(sx, sy, fx, fy))
+    test -= 1

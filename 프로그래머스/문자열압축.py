333eeb763d0557b423164db3e9357c701320d11e
Kevin <dlrghks4444@gmail.com>
2021-09-06 17:48:10 +0900

    문자열 압축의 개념 시간 복잡도 O(n^2) 이다.

--- 프로그래머스/문자열압축.py
+++ 프로그래머스/문자열압축.py
@@ -4,6 +4,8 @@ text3 = "abcabcabc"
 
 # 시간 복잡도 O(n^2)
 # 반복문을 이중으로 사용하였기 때문에 최악의 경우 n제곱의 시간이 든다.
+
+
 def study(s):
     check = []
     temp = ""
@@ -32,4 +34,42 @@ def study(s):
 
     return min(check)
 
-print(study(text3))
+
+# print(study(text3))
+
+
+def prac(s):
+    길이집합 = []
+    압축단어 = ""
+
+    if len(s) == 1:
+        return 1
+
+    for i in range(1, len(s) // 2 + 1):
+        압축단위 = s[:i]
+        압축횟수 = 1
+
+        for j in range(i, len(s), i):
+
+            # 내부에서 글자단위로 압축을 진행하는 부분
+            if 압축단위 == s[j: j + i]:
+                압축횟수 += 1
+
+            else:
+                if 압축횟수 == 1:
+                    압축횟수 = ""
+                압축단어 += str(압축횟수) + 압축단위
+                압축단위, 압축횟수 = s[j: j + i], 1
+
+        # 단위별 완료 된 경우 최종 압축단어를 만들어서 최종 길이를 측정하여 집합에 추가
+        if 압축횟수 == 1:
+            압축횟수 = ""
+        압축단어 += str(압축횟수) + 압축단위
+        길이집합.append(len(압축단어))
+        압축단어 = ""
+
+    print(길이집합)
+    return min(길이집합)
+
+
+print(prac(text3))

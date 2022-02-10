29af7145acf06d2d2889b0bbb44b9dd042810150
Kevin <dlrghks4444@gmail.com>
2022-02-11 08:52:48 +0900

    부분열 최대길이 값 구하기

---
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        dic, res, j = {}, 0, 0
+        for i in range(len(s)):
+            if s[i] not in dic:
+                dic[s[i]] = i
+            else:
+                j = max(j, dic[s[i]]+1)
+                dic[s[i]] = i
+            res = max(res, i-j+1)
+        return res

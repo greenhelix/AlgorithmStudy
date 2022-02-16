49128c4ee26f4fb4fd3672c401614c9a68048165
global_ik <helloglobal@example.com>
2022-02-16 13:44:18 +0900

    list median extend 사용

--- leetcode/MedianOfTwoSortedArrays.py
+++ leetcode/MedianOfTwoSortedArrays.py
@@ -1,6 +1,14 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1.extend(nums2)
-        nums1.sort()
-        return median(nums1)
+from statistics import median
+from typing import List
+
+
+nums1 = [1,2]
+nums2 = [2,3]
+
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    nums1.extend(nums2)
+    nums1.sort()
+    return median(nums1)
+
+print(findMedianSortedArrays(nums1, nums2))
         

efa350c8f4b7840763856e760b616ea69c02a617
global_ik <helloglobal@example.com>
2022-02-16 11:50:06 +0900

    list extend median 함수

---
+++ leetcode/MedianOfTwoSortedArrays.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        return median(nums1)
+        
